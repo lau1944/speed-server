0deb5a2551561b7bb3c3f15e6a5e79833cd9cd25
lau1944 <a954723219@gmail.com>
2022-08-27 23:41:39 -0400

    feat: change name

--- index.js
+++ index.js
@@ -3,10 +3,18 @@ const express = require('express');
 const port = process.env.PORT || 8080;
 const app = express();
 
-app.get('/download', function(req, res){
+app.get('/download', (req, res) => {
   const file = `${__dirname}/10M.iso`;
   res.download(file); 
 });
+
+app.post('/', (req, res) => {
+  res.status(200).send('Cool');
+})
+
+app.get('/', (req, res) => {
+  res.status(200).send('Cool');
+})
 
 app.listen(port, () => {
   console.log('Listening to port 8080');
